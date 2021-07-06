c92b45577b293c7e51df46817b388719ad265e46
herr-imko <58493296+herr-imko@users.noreply.github.com>
2021-07-06 22:30:41 +0300

    /

--- gulpfile.js
+++ gulpfile.js
@@ -13,67 +13,46 @@ const sourcemaps = require("gulp-sourcemaps")
 const GulpMem = require("gulp-mem")
 const imagemin = require("gulp-imagemin")
 const browserify = require('gulp-browserify')
+const argv = require('yargs')
+	.argv
 const gulpMem = new GulpMem()
-gulpMem.serveBasePath = "./build"
 gulpMem.logFn = null
+gulpMem.serveBasePath = "./build"
 
 function browserSyncF() {
-	return browserSync.init({
+	browserSync.init({
 		server: {
 			baseDir: "./build",
-			middleware: gulpMem.middleware,
+			middleware: argv.prod ? false : gulpMem.middleware
 		},
-		port: 3000,
+		port: 3000
 	})
 }
 
-function browserSyncFProd() {
-	return browserSync.init({
-		server: {
-			baseDir: "./build",
-		},
-		port: 3000,
+function emptyStream() {
+	return gulp.src('.', {
+		allowEmpty: true
 	})
 }
 
 function CSS() {
-	return (gulp.src("./src/style/style.scss")
+	return gulp.src("./src/style/style.scss")
 		.pipe(sourcemaps.init())
 		.pipe(sass({
 			errLogToConsole: true,
-			outputStyle: "expanded",
+			outputStyle: argv.prod ? "compressed" : "expanded",
 		}))
 		.on("error", console.error.bind(console))
-		// .pipe(autoPrefixer({
-		// 	cascade: true,
-		// 	overrideBrowserslist: ["last 3 versions"],
-		// }))
-		.pipe(rename({
-			extname: ".css"
-		}))
-		.pipe(sourcemaps.write("."))
-		.pipe(gulpMem.dest("./build/style/"))
-		.pipe(browserSync.stream()))
-}
-
-function CSSProd() {
-	return gulp.src("./src/style/style.scss")
-		// .pipe(sourcemaps.init())
-		.pipe(sass({
-			errLogToConsole: true,
-			outputStyle: "compressed",
-		}))
-		.on("error", console.error.bind(console))
-		.pipe(autoPrefixer({
+		.pipe(argv.prod ? autoPrefixer({
 			cascade: true,
 			overrideBrowserslist: ["last 3 versions"],
-		}))
+		}) : emptyStream())
 		.pipe(rename({
 			extname: ".css"
 		}))
-		.pipe(csso())
-		// .pipe(sourcemaps.write("."))
-		.pipe(gulp.dest("./build/style/"))
+		.pipe(argv.prod ? csso() : emptyStream())
+		.pipe(sourcemaps.write("."))
+		.pipe(argv.prod ? gulp.dest("./build/style/") : gulpMem.dest("./build/style/"))
 		.pipe(browserSync.stream())
 }
 
@@ -83,34 +62,16 @@ function JS() {
 		.pipe(browserify({
 			insertGlobals: true,
 		}))
+		.pipe(argv.prod ? uglify() : emptyStream())
 		.pipe(sourcemaps.write("."))
-		.pipe(gulpMem.dest("./build/script"))
-		.pipe(browserSync.stream())
-}
-
-function JSProd() {
-	return gulp.src("./src/script/script.js")
-		// .pipe(sourcemaps.init())
-		.pipe(browserify({
-			insertGlobals: true,
-		}))
-		.pipe(uglify())
-		// .pipe(sourcemaps.write("."))
-		.pipe(gulp.dest("./build/script"))
+		.pipe(argv.prod ? gulp.dest("./build/script") : gulpMem.dest("./build/script"))
 		.pipe(browserSync.stream())
 }
 
 function HTML() {
 	return gulp.src(["./src/*.html", "!./src/_*.html"])
 		.pipe(include())
-		.pipe(gulpMem.dest("./build"))
-		.pipe(browserSync.stream())
-}
-
-function HTMLProd() {
-	return gulp.src(["./src/*.html", "!./src/_*.html"])
-		.pipe(include())
-		.pipe(gulp.dest("./build"))
+		.pipe(argv.prod ? gulp.dest("./build") : gulpMem.dest("./build"))
 		.pipe(browserSync.stream())
 }
 
@@ -118,7 +79,7 @@ function copyAssets() {
 	return gulp.src("./src/assets/**/*", {
 			allowEmpty: true
 		})
-		.pipe(gulpMem.dest("./build/assets"))
+		.pipe(argv.prod ? gulp.dest("./build/assets") : gulpMem.dest("./build/assets"))
 		.pipe(browserSync.stream())
 }
 
@@ -126,7 +87,7 @@ function copyPlugins() {
 	return gulp.src("./src/plugins/**/*", {
 			allowEmpty: true
 		})
-		.pipe(gulpMem.dest("./build/plugins"))
+		.pipe(argv.prod ? gulp.dest("./build/plugins") : gulpMem.dest("./build/plugins"))
 		.pipe(browserSync.stream())
 }
 
@@ -141,74 +102,6 @@ function imageminF() {
 		.pipe(gulp.dest("./build/assets/img"))
 }
 
-function copyAssetsProd() {
-	return gulp.src("./src/assets/**/*", {
-			allowEmpty: true
-		})
-		.pipe(gulp.dest("./build/assets"))
-		.pipe(browserSync.stream())
-}
-
-function copyPluginsProd() {
-	return gulp.src("./src/plugins/**/*", {
-			allowEmpty: true
-		})
-		.pipe(gulp.dest("./build/plugins"))
-		.pipe(browserSync.stream())
-}
-
-function cleanCSS() {
-	return gulp.src("./build/style/", {
-			read: false,
-			allowEmpty: true
-		})
-		.pipe(clean())
-}
-
-function cleanHTML() {
-	return gulp.src("./build/*.html", {
-			read: false,
-			allowEmpty: true
-		})
-		.pipe(clean())
-}
-
-function cleanJS() {
-	return gulp.src("./build/script/*", {
-			read: false,
-			allowEmpty: true
-		})
-		.pipe(clean())
-}
-
-function cleanAssets() {
-	return gulp.src("./build/assets/*", {
-			read: false,
-			allowEmpty: true
-		})
-		.pipe(clean())
-}
-
-function cleanPlugins() {
-	return gulp.src("./build/plugins/*", {
-			read: false,
-			allowEmpty: true
-		})
-		.pipe(clean())
-}
-
-function cleanBuild() {
-	return gulp.src("./build", {
-			read: false,
-			allowEmpty: true
-		})
-		.pipe(clean())
-}
-
-function reload() {
-	return browserSync.stream()
-}
-
 function watch() {
 	gulp.watch("./src/*.html", gulp.series(HTML))
 	gulp.watch("./src/script/*", gulp.series(JS))
@@ -217,21 +110,12 @@ function watch() {
 	gulp.watch("./src/plugins/**/*", gulp.series(copyPlugins))
 }
 
-function watchProd() {
-	gulp.watch("./src/*.html", gulp.series(HTMLProd))
-	gulp.watch("./src/script/*", gulp.series(JSProd))
-	gulp.watch("./src/style/**/*", gulp.series(CSSProd))
-	gulp.watch("./src/assets/**/*", gulp.series(copyAssetsProd))
-	gulp.watch("./src/plugins/**/*", gulp.series(copyPluginsProd))
-}
-
 function ttfToWoffF() {
 	return gulp.src(['./src/assets/font/*.ttf'])
 		.pipe(clean())
 		.pipe(ttf2woff2())
 		.pipe(gulp.dest('./src/assets/font/'))
 }
-exports.default = gulp.series(gulp.parallel(CSS, JS, HTML, copyAssets, copyPlugins), browserSyncF, gulp.parallel(watch))
-exports.prod = gulp.series(cleanBuild, gulp.parallel(CSSProd, JSProd, HTMLProd, copyAssetsProd, copyPluginsProd), browserSyncFProd, gulp.parallel(watchProd))
+exports.default = gulp.series(gulp.parallel(CSS, JS, HTML, copyAssets, copyPlugins), gulp.parallel(browserSyncF, watch))
 exports.imagemin = gulp.series(imageminF)
 exports.ttfToWoff = gulp.series(ttfToWoffF)
