bbcae0387749f27cc31b494a20a813f28397b459
herr-imko <58493296+herr-imko@users.noreply.github.com>
2021-09-30 11:21:42 +0300

    /

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,28 @@
 import gulp from "gulp"
-import GulpSass from 'gulp-sass'
-import Sass from 'sass'
+import GulpSass from "gulp-sass"
+import Sass from "sass"
 import autoPrefixer from "gulp-autoprefixer"
 import browserSync from "browser-sync"
-import replace from 'gulp-replace'
-import ttf2woff2 from 'gulp-ttf2woff2'
+import replace from "gulp-replace"
+import ttf2woff2 from "gulp-ttf2woff2"
 import uglify from "gulp-uglify"
 import include from "gulp-include"
 import clean from "gulp-clean"
 import csso from "gulp-csso"
-import buffer from 'vinyl-buffer'
+import buffer from "vinyl-buffer"
 import sourcemaps from "gulp-sourcemaps"
 import GulpMem from "gulp-mem"
 import imagemin from "gulp-imagemin"
-import browserify from 'browserify'
-import source from 'vinyl-source-stream'
-import flatmap from 'gulp-flatmap'
-import path from 'path'
-import cache from 'gulp-cached'
-import yargs from 'yargs'
-import notify from 'gulp-notify'
+import browserify from "browserify"
+import source from "vinyl-source-stream"
+import flatmap from "gulp-flatmap"
+import path from "path"
+import cache from "gulp-cached"
+import yargs from "yargs"
+import notify from "gulp-notify"
 import {
 	hideBin
-} from 'yargs/helpers'
+} from "yargs/helpers"
 const argv = yargs(hideBin(process.argv))
 	.argv,
 	sass = GulpSass(Sass),
@@ -48,7 +48,7 @@ function emptyTask(cb) {
 }
 
 function emptyBuffer() {
-	return gulp.src('neverUsedName', {
+	return gulp.src("neverUsedName", {
 		allowEmpty: true
 	})
 }
@@ -59,10 +59,10 @@ function CSS() {
 		.pipe(sass.sync({
 				errLogToConsole: true,
 				outputStyle: argv.min ? "compressed" : "expanded",
-				includePaths: ['node_modules']
+				includePaths: ["node_modules"]
 			})
-			.on('error', sass.logError)
-			.on('error', notify.onError({
+			.on("error", sass.logError)
+			.on("error", notify.onError({
 				message: "<%= error.message %>",
 				title: "SASS"
 			})))
@@ -71,24 +71,24 @@ function CSS() {
 			overrideBrowserslist: ["last 3 versions"],
 		}))
 		.pipe(argv.min ? csso() : emptyBuffer())
-		.pipe(argv.ram ? emptyBuffer() : replace('/src/', '/'))
+		.pipe(argv.ram ? emptyBuffer() : replace("/src/", "/"))
 		.pipe(sourcemaps.write("."))
 		.pipe(argv.ram ? gulpMem.dest("./build/src/assets/style/") : gulp.dest("./build/assets/style/"))
 		.pipe(browserSync.stream())
 }
 
 function JS() {
-	return gulp.src(['./src/assets/script/*.js', '!./src/assets/script/_*.js'])
+	return gulp.src(["./src/assets/script/*.js", "!./src/assets/script/_*.js"])
 		.pipe(flatmap(function (stream, file) {
 			return browserify(`./src/assets/script/${path.basename(file.path)}`, {
 					debug: true,
 				})
 				.bundle()
-				.on('error', function (error) {
+				.on("error", function (error) {
 					console.log(error.message)
-					this.emit('end')
+					this.emit("end")
 				})
-				.on('error', notify.onError({
+				.on("error", notify.onError({
 					message: "<%= error.message %>",
 					title: "JS"
 				}))
@@ -99,7 +99,7 @@ function JS() {
 			loadMaps: true
 		}))
 		.pipe(argv.min ? uglify() : emptyBuffer())
-		.pipe(sourcemaps.write('./'))
+		.pipe(sourcemaps.write("./"))
 		.pipe(argv.ram ? gulpMem.dest("./build/src/assets/script/") : gulp.dest("./build/assets/script/"))
 		.pipe(browserSync.stream())
 }
@@ -108,12 +108,12 @@ function HTML() {
 	return gulp.src(["./src/*.html", "!./src/_*.html"])
 		.pipe(flatmap(function (stream, file) {
 			return stream.pipe(include()
-					.on('error', console.log)
-					.on('error', notify.onError({
+					.on("error", console.log)
+					.on("error", notify.onError({
 						message: "<%= error.message %>",
 						title: "HTML"
 					})))
-				.pipe(argv.ram ? emptyBuffer() : replace('/src/', '/'))
+				.pipe(argv.ram ? emptyBuffer() : replace("/src/", "/"))
 				.pipe(argv.separate ? replace("style.css", `${path.basename(file.path , ".html")}.css`) : emptyBuffer())
 				.pipe(argv.separate ? replace("script.js", `${path.basename(file.path, ".html")}.js`) : emptyBuffer())
 		}))
@@ -125,7 +125,7 @@ function copyStatic() {
 	return gulp.src("./src/assets/static/**/*", {
 			allowEmpty: true
 		})
-		.pipe(cache('static'))
+		.pipe(cache("static"))
 		.pipe(argv.ram ? gulpMem.dest("./build/src/assets/static/") : gulp.dest("./build/assets/static/"))
 		.pipe(browserSync.stream())
 }
@@ -155,11 +155,11 @@ function cleanBuild() {
 }
 
 function ttfToWoff() {
-	return gulp.src(['./src/assets/static/font/*.ttf'])
+	return gulp.src(["./src/assets/static/font/*.ttf"])
 		.pipe(clean())
 		.pipe(ttf2woff2())
-		.pipe(gulp.dest('./src/assets/static/font/'))
+		.pipe(gulp.dest("./src/assets/static/font/"))
 }
-gulp.task('default', gulp.series(argv.ram ? emptyBuffer : cleanBuild, gulp.parallel(CSS, JS, HTML, copyStatic), argv.watch ? gulp.parallel(watch, browserSyncInit) : emptyBuffer))
-gulp.task('imagemin', minimizeImgs)
-gulp.task('ttfToWoff', ttfToWoff)
+gulp.task("default", gulp.series(argv.ram ? emptyBuffer : cleanBuild, gulp.parallel(CSS, JS, HTML, copyStatic), argv.watch ? gulp.parallel(watch, browserSyncInit) : emptyBuffer))
+gulp.task("imagemin", minimizeImgs)
+gulp.task("ttfToWoff", ttfToWoff)
