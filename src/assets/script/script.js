97433f5a2d98fabaafe1d67b56dd1a4039d3a11b
herr-imko <58493296+herr-imko@users.noreply.github.com>
2022-06-16 13:34:34 +0300

    /

--- src/assets/script/script.js
+++ src/assets/script/script.js
@@ -1,125 +1,125 @@
-import Splide from "@splidejs/splide";
-import "fslightbox";
-document.addEventListener('DOMContentLoaded', function () { });
+import Splide from "@splidejs/splide"
+import "fslightbox"
+document.addEventListener('DOMContentLoaded', function () { })
 
 function initSpoilers() {
 	document.querySelectorAll('.spoiler')
 		.forEach(spoiler => {
-			let slideToggle = makeSlideToggle(spoiler.querySelector(".spoiler-content"));
+			let slideToggle = makeSlideToggle(spoiler.querySelector(".spoiler-content"))
 			spoiler.querySelector('.spoiler-toggler')
 				.addEventListener('click', function () {
-					spoiler.classList.toggle("spoiler-opened");
-					slideToggle();
-				});
+					spoiler.classList.toggle("spoiler-opened")
+					slideToggle()
+				})
 			if (spoiler.dataset.spoilerDefaultOpened) {
-				slideToggle();
+				slideToggle()
 			}
-		});
+		})
 }
 
 function initQuantity() {
 	function initQuantityItem(quantity) {
-		let input = quantity.querySelector('input[type="number"]');
-		let btnPlus = quantity.querySelector(".quantity--button:last-of-type");
-		let btnMinus = quantity.querySelector(".quantity--button:first-of-type");
+		let input = quantity.querySelector('input[type="number"]')
+		let btnPlus = quantity.querySelector(".quantity--button:last-of-type")
+		let btnMinus = quantity.querySelector(".quantity--button:first-of-type")
 		new Array(btnPlus, btnMinus)
 			.forEach((btn) => {
 				btn.addEventListener("click", function () {
-					let newValue = +input.value + +this.dataset.value;
-					console.log(input.min, input.max);
+					let newValue = +input.value + +this.dataset.value
+					console.log(input.min, input.max)
 					if (input.min == "" || input.max == "") {
-						input.value = newValue;
-						return;
+						input.value = newValue
+						return
 					}
 					if (newValue < input.min) {
-						btnMinus.setAttribute("disabled", "");
+						btnMinus.setAttribute("disabled", "")
 					} else if (newValue > input.max) {
-						btnPlus.setAttribute("disabled", "");
+						btnPlus.setAttribute("disabled", "")
 					} else if (newValue == input.max) {
-						input.value = newValue;
-						btnPlus.setAttribute("disabled", "");
+						input.value = newValue
+						btnPlus.setAttribute("disabled", "")
 					} else if (newValue == input.min) {
-						input.value = newValue;
-						btnMinus.setAttribute("disabled", "");
+						input.value = newValue
+						btnMinus.setAttribute("disabled", "")
 					} else {
-						input.value = newValue;
-						btnPlus.removeAttribute("disabled");
-						btnMinus.removeAttribute("disabled");
+						input.value = newValue
+						btnPlus.removeAttribute("disabled")
+						btnMinus.removeAttribute("disabled")
 					}
-				});
-			});
+				})
+			})
 		input.addEventListener("change", function () {
 			if (!input.checkValidity()) {
 				if (input.value > input.max) {
-					input.value = input.max ? input.max : 0;
+					input.value = input.max ? input.max : 0
 				}
 				if (input.value < input.min) {
-					input.value = input.min ? input.min : 0;
+					input.value = input.min ? input.min : 0
 				}
 			}
-		});
+		})
 	}
 	document.querySelectorAll('.quantity')
-		.forEach(initQuantityItem);
-	window.initQuantityItem = initQuantityItem;
+		.forEach(initQuantityItem)
+	window.initQuantityItem = initQuantityItem
 }
 
 function makeSlideToggle(elem) {
 	let initialHeight = getComputedStyle(elem)
-		.maxHeight;
+		.maxHeight
 	return function () {
 		if (event) {
-			event.preventDefault();
+			event.preventDefault()
 		}
 		if (elem.style.maxHeight == "") {
-			elem.focus();
-			elem.style.maxHeight = `${elem.scrollHeight}px`;
+			elem.focus()
+			elem.style.maxHeight = `${elem.scrollHeight}px`
 		} else if (elem.style.maxHeight != initialHeight) {
-			elem.blur();
-			elem.style.maxHeight = initialHeight;
+			elem.blur()
+			elem.style.maxHeight = initialHeight
 		} else {
-			elem.focus();
-			elem.style.maxHeight = `${elem.scrollHeight}px`;
+			elem.focus()
+			elem.style.maxHeight = `${elem.scrollHeight}px`
 		}
-	};
+	}
 }
 
 function initTabs() {
 	document.querySelectorAll(".tabs")
 		.forEach(tabgroup => {
-			let controls = tabgroup.querySelectorAll(".tabs-control");
-			let contents = Array.from(tabgroup.querySelectorAll(".tabs-content"));
+			let controls = tabgroup.querySelectorAll(".tabs-control")
+			let contents = Array.from(tabgroup.querySelectorAll(".tabs-content"))
 			controls.forEach(control => {
 				control.addEventListener("click", function () {
 					contents.forEach(content => {
-						content.classList.remove("active");
-					});
-					controls.forEach(control => {
-						control.classList.remove("active");
-					});
-					contents.find(function (content) {
-						return content.id == control.dataset.tabTarget;
+						content.classList.remove("active")
 					})
-						.classList.add("active");
-					control.classList.add("active");
-				});
-			});
-		});
+					controls.forEach(control => {
+						control.classList.remove("active")
+					})
+					contents.find(function (content) {
+						return content.id == control.dataset.tabTarget
+					})
+						.classList.add("active")
+					control.classList.add("active")
+				})
+			})
+		})
 }
 
 function toggleNoscrollBody() {
 	if (document.body.classList.contains('noscroll')) {
-		document.body.classList.remove('noscroll');
+		document.body.classList.remove('noscroll')
 		window.scrollTo({
 			top: document.body.style.getPropertyValue('--scroll-position')
 				.replace('px', ""),
 			left: 0,
 			behavior: "instant"
-		});
+		})
 	} else {
-		document.body.style.setProperty("--scroll-position", `${window.pageYOffset}px`);
-		document.body.style.setProperty("--scrollbar-width", `${window.innerWidth - document.documentElement.clientWidth}px`);
-		document.body.classList.add('noscroll');
+		document.body.style.setProperty("--scroll-position", `${window.pageYOffset}px`)
+		document.body.style.setProperty("--scrollbar-width", `${window.innerWidth - document.documentElement.clientWidth}px`)
+		document.body.classList.add('noscroll')
 	}
 }
 
@@ -128,57 +128,57 @@ function initPopups() {
 		.forEach(function (popup) {
 			function makeClosePopup() {
 				return function () {
-					popup.blur();
-				};
+					popup.blur()
+				}
 			}
 
 			function makeOpenPopup() {
 				return function () {
-					popup.focus();
-				};
+					popup.focus()
+				}
 			}
-			window[`closePopup_${popup.id}`] = makeClosePopup();
-			window[`openPopup_${popup.id}`] = makeOpenPopup();
+			window[`closePopup_${popup.id}`] = makeClosePopup()
+			window[`openPopup_${popup.id}`] = makeOpenPopup()
 			document.querySelectorAll(`.popup-opener[data-target="${popup.id}"]`)
 				.forEach(function (opener) {
 					opener.addEventListener('click', function () {
-						popup.focus();
-						event.preventDefault();
-						event.stopPropagation();
-					});
-				});
+						popup.focus()
+						event.preventDefault()
+						event.stopPropagation()
+					})
+				})
 			popup.querySelectorAll("input, textarea")
 				.forEach(input => {
 					input.addEventListener("focusout", () => {
-						popup.focus();
-					});
-				});
+						popup.focus()
+					})
+				})
 			popup.addEventListener('click', function () {
-				popup.blur();
-			});
+				popup.blur()
+			})
 			popup.querySelector(".popup-inner")
 				.addEventListener('click', function () {
-					event.stopPropagation();
-				});
+					event.stopPropagation()
+				})
 			popup.addEventListener('focus', function () {
 				if (!this.classList.contains('show')) {
-					toggleNoscrollBody();
-					this.classList.add("show");
+					toggleNoscrollBody()
+					this.classList.add("show")
 				}
-			});
+			})
 			popup.addEventListener('focusout', function (event) {
 				setTimeout(() => {
 					if (!this.matches(":focus-within") && this.classList.contains('show')) {
-						toggleNoscrollBody();
-						this.classList.remove("show");
+						toggleNoscrollBody()
+						this.classList.remove("show")
 					}
-				}, 100);
-			});
+				}, 100)
+			})
 			popup.querySelectorAll(".popup-closer")
 				.forEach(closer => {
 					closer.addEventListener("click", function () {
-						popup.blur();
-					});
-				});
-		});
+						popup.blur()
+					})
+				})
+		})
 }
