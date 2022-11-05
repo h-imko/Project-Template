a999a8fcbbf91ae21dc8fa0efca6476f46cf8f58
herr-imko <58493296+herr-imko@users.noreply.github.com>
2022-11-06 02:08:08 +0300

    Revert "Merge commit '0c1b014b4d1dbe6f43c189cea904cd4e553eec0e'"
    This reverts commit ae0e32a00cea1a2bc377b20f1cef76d5b8c7c288, reversing
    changes made to bf105a3675b6cd136ea2297bf21ba06702522ff4.

--- src/assets/script/_Spoiler.js
+++ src/assets/script/_Spoiler.js
@@ -8,18 +8,35 @@ class Spoiler {
 		this.spoiler = target
 		this.toggler = target.querySelector('.spoiler__toggler')
 		this.content = target.querySelector('.spoiler__content')
-		this.controller = target.querySelector('.spoiler__controller')
-		this.isNative = this.toggler.hasAttribute("for")
-
-		if (!this.isNative) {
-			this.initToggler()
-		}
+		this.activeClass = "is-active"
+		this.nestedSpoilers = target.querySelectorAll(".spoiler")
+		this.duration = +target.dataset?.spoilerDuration || 0
+		this.initClick()
+		this.injectDuration()
 	}
 
-	initToggler() {
-		this.toggler.addEventListener("click", () => {
-			this.controller.checked = !this.controller.checked
+	slideToggle() {
+		this.content.style.setProperty("max-height", `${this.content.scrollHeight}px`)
+		setTimeout(() => {
+			this.content.style.removeProperty("max-height")
+		}, this.duration)
+
+	}
+
+	injectDuration() {
+		this.spoiler.style.setProperty("--transition-duration", `${this.duration / 1000}s`)
+	}
+
+	initClick() {
+		this.toggler.addEventListener('click', () => {
+			this.slideToggle()
+			this.toggler.classList.toggle(this.activeClass)
+			this.content.classList.toggle(this.activeClass)
+			this.spoiler.classList.toggle(this.activeClass)
 		})
+		if (this.spoiler.dataset?.spoilerDefaultOpened) {
+			this.slideToggle()
+		}
 	}
 }
 
--- src/assets/script/script.js
+++ src/assets/script/script.js
@@ -10,8 +10,8 @@ document.addEventListener('DOMContentLoaded', function () {
 	initPopups()
 	// initTabs()
 	// initPhoneMask()
-	initQuantity()
-	initSpoilers()
+	// initQuantity()
+	// initSpoilers()
 	// initInputControllerGroups()
 })
 
