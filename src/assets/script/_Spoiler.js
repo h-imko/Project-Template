591e859568541f9e811e69647b14ae429c3a32f2
herr-imko <58493296+herr-imko@users.noreply.github.com>
2023-02-27 09:42:34 +0300

    /

--- src/assets/script/_Spoiler.js
+++ src/assets/script/_Spoiler.js
@@ -22,21 +22,19 @@ class Spoiler {
 				height = `${this.content.scrollHeight}px`
 
 				if (style.getPropertyValue("--scroll-height") != height) {
-					style.setProperty("--scroll-height", height)
 					style.setProperty("--transition", `none`)
+					style.setProperty("--scroll-height", height)
 					requestAnimationFrame(() => {
 						style.removeProperty("--transition")
 					})
 				}
 
 				requestAnimationFrame(() => {
-					requestAnimationFrame(() => {
-						this.togglers.forEach(toggler => {
-							toggler.classList.toggle(this.activeClass)
-						})
-						this.content.classList.toggle(this.activeClass)
-						this.spoiler.classList.toggle(this.activeClass)
+					this.togglers.forEach(toggler => {
+						toggler.classList.toggle(this.activeClass)
 					})
+					this.content.classList.toggle(this.activeClass)
+					this.spoiler.classList.toggle(this.activeClass)
 				})
 			})
 		})
